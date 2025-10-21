2650d5827bc59dfd3eaa460a798669a1506d49b3
Sergio Silva <ssilva@updevelop.com.co>
2025-10-20 20:31:07 -0500

    feat: setup Spring Boot project (Web, DevTools, Lombok, Validation) and /health endpoint

---
+++ src/main/java/com/sergiosilva/aa5ev01/Aa5Ev01ServiciosWebApplication.java
@@ -0,0 +1,12 @@
+package com.sergiosilva.aa5ev01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa5Ev01ServiciosWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aa5Ev01ServiciosWebApplication.class, args);
+	}
+}
